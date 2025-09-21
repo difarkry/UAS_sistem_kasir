a6f4ae6a0f1b579ee2fb1a3353904eb1818769e5
difarkry <Diodisiusf.k123@gmail.com>
2025-09-21 16:25:14 +0700

    Menambah peringatan jika stok kosong

--- FILE/lihat_barang.py
+++ FILE/lihat_barang.py
@@ -14,11 +14,17 @@ def lihat():
     lihat = cursor.fetchone()
     cursor.execute("SELECT * FROM BARANG")
     semua =  cursor.fetchall()
+    cursor.execute("SELECT * FROM barang WHERE stock = 0")
+    cek = cursor.fetchall()
     if lihat is None:
         print("\nANDA BELUM MENGISI DATA\n")
         t.sleep(3)
-        return
+        return 
     elif lihat is not None:
+        
         for id,nama,harga,stock in semua:
             print(f"ID : {id:<5}\tNAMA : {nama:<10}\tHARGA : {harga:<10}\t STOCK : {stock:<5}".upper())
+        print("")    
+        for nama in cek:    
+            print(f"Peringatan Stock {nama[1]} sedang kosong")
         t.sleep(10)    
